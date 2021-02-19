2136c290ca9c3be875e24073a590f98c957d32ab
Vincent <vincent.tan.1998@gmail.com>
2021-02-19 18:56:07 -0500

    navbar and interactivity

--- main.js
+++ main.js
@@ -6,7 +6,16 @@ var scroll = window.requestAnimationFrame ||
 // scroll animation related elements
 var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
 
+sectionToNavbar = {"landing":"navbar-landing","about":"navbar-about","projects":"navbar-projects","system":"navbar-system","contact":"navbar-contact"};
+
+// var navbarIds = ["navbar-landing","navbar-about","navbar-projects","navbar-system","navbar-contact"];
+var sectionIds = ["#landing","#about","#projects","#system","#contact"];
+var sections = document.querySelectorAll(sectionIds);
+
+alert('starto');
+
 function loop() {
+    // animate
     elementsToShow.forEach(function (element) {
         if (isElementInViewport(element)) {
             element.classList.add('is-visible');
@@ -14,6 +23,18 @@ function loop() {
             element.classList.remove('is-visible');
         }
     });
+
+    // navbar
+    sections.forEach(function (element){
+        if (isElementInViewport(element)){
+            // alert(sectionToNavbar[element.id]);
+            // alert(document.getElementById(sectionToNavbar[element.id]));
+            document.getElementById(sectionToNavbar[element.id]).classList.add('active');
+        }else{
+            document.getElementById(sectionToNavbar[element.id]).classList.remove('active');
+
+        }
+    })
 
     scroll(loop);
 }
